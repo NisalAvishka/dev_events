5c0037972f1be6882ebc2eaa10eccbf724104b00
Nisal Balasooriya <nisalavishka2001@gmail.com>
2025-11-01 23:37:12 +0530

    Event Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import { events } from "@/lib/constants";
-import EventCard from "./components/EventCard";
-import { ExploreBtn } from "./components/ExploreBtn";
+import EventCard from "../components/EventCard";
+import { ExploreBtn } from "../components/ExploreBtn";
+import { IEvent } from "@/database";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 
-const Home = () => {
+const Home = async () => {
+
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const {events} = await response.json();
+ 
+
   return (
     <section>
       <h1 className="text-center">
@@ -17,7 +24,7 @@ const Home = () => {
       <div className="mt-20 space-y-7">
         <h3>Featured Events</h3>
         <ol className="events">
-          {events.map((event) => (
+          {events && events.length > 0 && events.map((event: IEvent) => (
             <li key={event.title}>
             <EventCard {...event}/></li>
           ))}
