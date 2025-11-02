d3bf26102eeef205485971f1da8b362284c5ff6d
Nisal Balasooriya <nisalavishka2001@gmail.com>
2025-11-02 19:15:54 +0530

    Fix build errors

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -2,18 +2,26 @@
 
 import connectToDatabase from "@/lib/mongodb";
 import { Event } from "@/database";
+import { unstable_cache } from "next/cache";
 
-export const getSimilarEvents = async (slug: string) => {
-  try {
-    await connectToDatabase();
+export const getSimilarEvents = unstable_cache(
+  async (slug: string) => {
+    try {
+      await connectToDatabase();
 
-    const event = await Event.findOne({ slug });
+      const event = await Event.findOne({ slug });
 
-    return await Event.find({
-      _id: { $ne: event?._id },
-      tags: { $in: event?.tags || [] },
-    }).lean();
-  } catch {
-    return [];
+      return await Event.find({
+        _id: { $ne: event?._id },
+        tags: { $in: event?.tags || [] },
+      }).lean();
+    } catch {
+      return [];
+    }
+  },
+  ["similar-events"],
+  {
+    revalidate: 3600, // Cache for 1 hour
+    tags: ["events"],
   }
-};
+);
